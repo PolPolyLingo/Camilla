205b92da0a45ae04ab988a8792f5916cb2a90c56
Nao1215 <n.chika156@gmail.com>
2021-08-04 01:05:02 +0900

    Add install process for gettext package.

--- scripts/setupDevelopmentEnvForUbuntu.sh
+++ scripts/setupDevelopmentEnvForUbuntu.sh
@@ -2,7 +2,7 @@
 
 PACKAGES="git valac valac-bin libvala-0.48-dev libvalacodegen-0.48-0 valac-0.48-vapi \
           meson ninja-build libglib2.0-dev build-essential gzip pandoc uncrustify \
-          gir1.2-gee-0.8 libgee-0.8-2 libgee-0.8-dev"
+          gir1.2-gee-0.8 libgee-0.8-2 libgee-0.8-dev gettext"
 
 function isUbuntu() {
     cat /etc/os-release | grep "NAME=\"Ubuntu\""
