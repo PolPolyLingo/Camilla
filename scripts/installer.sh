11e937956b48cf2ec63f9cb826d7bc72c231c7cd
Nao1215 <n.chika156@gmail.com>
2021-08-03 15:02:28 +0900

    Bug fix and feature addition: The target directory for installation could not be created. Also added a process to install the log config file.

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -7,6 +7,8 @@ MAN_PAGES_LANG="ja en"
 DOC_INSTALL_DIR="/usr/share/doc/camilla"
 LICENSE="${ROOT_DIR}/LICENSE"
 NOTICE="${ROOT_DIR}/NOTICE"
+CONFIG_INSTALL_DIR="/usr/share/camilla"
+LOG_CONFIG="${ROOT_DIR}/data/camilla-log4vala.conf"
 
 function errMsg() {
     local message="$1"
@@ -28,14 +30,18 @@ function installManPages() {
 
     for lang in ${MAN_PAGES_LANG}
     do
+        mkdir -p /usr/share/man/man1/.
+        mkdir -p /usr/share/man/${lang}/man1/.
+
         local man_pages=$(find ${MAN_DIR}/ -name "*.gz" | grep "/${lang}/")
         for man_page in ${man_pages}
         do
-            echo "Install ${man_page}" 
             if [ "${lang}" = "en" ]; then
-                install -m 0644 -D ${man_page} /usr/share/man/man1/.
+                echo "Install ${man_page} at /usr/share/man/man1" 
+                install -m 0644 ${man_page} /usr/share/man/man1/.
             else
-                install -m 0644 -D ${man_page} /usr/share/man/${lang}/man1/.
+                echo "Install ${man_page} at /usr/share/man/${lang}/man1" 
+                install -m 0644 ${man_page} /usr/share/man/${lang}/man1/.
             fi
         done
     done
@@ -43,10 +49,18 @@ function installManPages() {
 
 function installLicense() {
     echo "Install LICENSE and NOTICE at ${DOC_INSTALL_DIR}"
-    install -m 0644 -D ${LICENSE} ${DOC_INSTALL_DIR}
-    install -m 0644 -D ${NOTICE} ${DOC_INSTALL_DIR}
-}   
+    mkdir -p ${DOC_INSTALL_DIR}
+    install -m 0644 ${LICENSE} ${DOC_INSTALL_DIR}
+    install -m 0644 ${NOTICE} ${DOC_INSTALL_DIR}
+}
+
+function installConfigFile() {
+    echo "Install log config file at ${CONFIG_INSTALL_DIR}"
+    mkdir -p ${CONFIG_INSTALL_DIR}
+    install -m 0644 ${LOG_CONFIG} ${CONFIG_INSTALL_DIR}
+}
 
 isRoot
+installConfigFile
 installManPages
 installLicense
